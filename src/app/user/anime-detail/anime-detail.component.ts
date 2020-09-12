943356dcba8e83c23f25165eb4a0c6df05011db8
Victor Henrique S Cabral <victor.cabral@ADM.UNIP.BR>
2020-09-12 13:53:14 -0300

    reduzir fonte do titulo pelo tamanho do texto

--- src/app/user/anime-detail/anime-detail.component.ts
+++ src/app/user/anime-detail/anime-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AnimeDetailService } from './anime-detail.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Anime } from 'src/app/shared/models/anime.model';
@@ -9,9 +9,11 @@ import { Musica } from 'src/app/shared/models/musica.model';
   templateUrl: './anime-detail.component.html',
   styleUrls: ['./anime-detail.component.css']
 })
-export class AnimeDetailComponent implements OnInit {
+export class AnimeDetailComponent implements OnInit{
 
   constructor(private animeDetailService: AnimeDetailService, private router: Router, private route: ActivatedRoute) { }
+
+  @ViewChild("title") titulo: ElementRef;
 
   idAnime: string;
   anime: Anime;
@@ -21,9 +23,10 @@ export class AnimeDetailComponent implements OnInit {
     if(this.idAnime){
       this.animeDetailService.getAnimeDetailsById(this.idAnime).subscribe(res => {
         this.anime = res;
+        this.titulo.nativeElement.style.fontSize = this.reduzirTamanhoFonteTitulo(this.anime.nome) + "px";
       });
     } else {
-      this.router.navigate(['user/list']);
+      this.router.navigate(['user']);
     }
   }
 
@@ -47,5 +50,16 @@ export class AnimeDetailComponent implements OnInit {
     tempLi.setAttribute(dropdown.attributes[0].name,"");
     tempLi.innerText = texto;
     dropdown.appendChild(tempLi);
+  }
+
+  reduzirTamanhoFonteTitulo(nome: string){
+    let tamanhoFontePadrao = 36;
+    let quantidadeDeLinhas = Math.floor(nome.length/12);
+    if(quantidadeDeLinhas > 1){
+      const porcentagemTamanhoIdeal = 100 - (10 * quantidadeDeLinhas);
+      let tamanhoFonte  = tamanhoFontePadrao * (porcentagemTamanhoIdeal / 100);
+      return tamanhoFonte;
+    }
+    return tamanhoFontePadrao;
   }
 }
