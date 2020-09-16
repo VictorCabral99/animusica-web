858ca480aac43fd3ba8fcefd6b46533421dc07ca
Victor Henrique S Cabral <victor.cabral@ADM.UNIP.BR>
2020-09-15 22:07:01 -0300

    previsualizar imagem do anime

--- src/app/admin/insert-anime/insert-anime.component.ts
+++ src/app/admin/insert-anime/insert-anime.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsertAnimeComponent implements OnInit {
 
+  caminhoImagem;
+  imgURL: any;
+  mensagem: string;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  previsualizarImagem(file){
+    if(file.length === 0){
+      return;
+    }
+
+    var mimeType = file[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.mensagem = "Apenas imagens são permitidas."
+      return;
+    }
+
+    var reader = new FileReader();
+    this.caminhoImagem = file;
+    reader.readAsDataURL(file[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    }
   }
 
 }
